5a56dbd94468d38d7b047d3cb06b3cf1a89f9bab
WolverinLando <orlandourbanotrejo@gmail.com>
2024-03-11 11:39:57 -0600

    Archivo para eliminar personas de la base de datos

---
+++ eliminar_persona.php
@@ -0,0 +1,16 @@
+<?php
+include("conexion/conexion.php");
+$nombre = $_GET['nombre_persona'];
+echo $nombre;
+
+# Llamada al procedure
+$sql = "CALL proc_eliminar_persona('$nombre')";
+
+if (mysqli_query($conexion,$sql)){
+        header("location:consultar_personas.php");
+} else {
+   echo "Error al eliminar la base de datos";
+} 
+mysqli_close;
+?>
+
