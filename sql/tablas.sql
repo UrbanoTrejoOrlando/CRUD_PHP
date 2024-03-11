c6e3d818ea4b1bca78b5d08935e7fbd60a1fc6fc
WolverinLando <orlandourbanotrejo@gmail.com>
2024-03-11 11:50:03 -0600

    Diseño de tablas

---
+++ sql/tablas.sql
@@ -0,0 +1,11 @@
+CREATE TABLE persona (
+	id_persona MEDIUMINT AUTO_INCREMENT,
+	nombre VARCHAR(30) NOT NULL,
+	apellido_Paterno VARCHAR(30) NOT NULL,
+	apellido_Materno VARCHAR(30) NOT NULL,
+	edad MEDIUMINT NOT NULL,
+	fecha_Nacimiento DATE NOT NULL,
+	sexo BINARY NOT NULL,
+	estatus CHAR(1) DEFAULT '1' NOT NULL,
+	PRIMARY KEY(id_persona)
+);
