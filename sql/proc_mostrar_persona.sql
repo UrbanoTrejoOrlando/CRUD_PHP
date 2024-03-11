f50dfe59cb9219c7a0caec1aedd67fd8bd2802ce
WolverinLando <orlandourbanotrejo@gmail.com>
2024-03-11 11:48:47 -0600

    Procedure para mostrar personas

---
+++ sql/proc_mostrar_persona.sql
@@ -0,0 +1,22 @@
+DROP PROCEDURE IF EXISTS proc_mostrar_persona;
+
+DELIMITER //
+CREATE PROCEDURE proc_mostrar_persona (
+	IN p_nombre VARCHAR(30)
+)
+BEGIN 
+	SELECT
+		nombre,
+		apellido_Paterno,
+		apellido_Materno,
+		edad,
+		fecha_Nacimiento,
+		CASE sexo
+			WHEN 1 THEN 'Masculino'
+				ELSE 'Femenino'  
+		END AS 'Sexo'
+	FROM persona WHERE nombre = p_nombre;
+END //
+DELIMITER ;
+
+CALL proc_mostrar_persona('Ernesto');
