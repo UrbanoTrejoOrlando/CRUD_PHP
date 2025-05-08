b21b315316afc160a5e384349ca7f552377ee607
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-07 22:08:33 -0600

    Modificaciones

--- eliminar_persona.php
+++ eliminar_persona.php
@@ -9,6 +9,7 @@ echo $nombre;
 # Llamada al procedure
 $sql = "CALL proc_eliminar_persona('$nombre')";
 
+// Verificar si la conexion es correcta
 if (mysqli_query($conexion,$sql)){
         header("location:consultar_personas.php");
 } else {
