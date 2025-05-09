7f1ee5ba1028769ae9a31f8f98c15e7926873e88
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-08 21:33:25 -0600

    Modificaciones

--- sql/usuario.sql
+++ sql/usuario.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE crud_php;
--- Creacion de un usuario
 CREATE USER 'php'@'%' IDENTIFIED BY 'conexion';
 --- Otorgrar los permisos al usuario sobre la base de datos
 GRANT ALL PRIVILEGES ON crud_php.* TO 'php'@'%';
